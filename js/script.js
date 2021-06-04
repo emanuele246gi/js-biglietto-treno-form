eab0a224cd03c8fdb8e0c19cbb04c6694895bb9a
Emanuele <emanuele65b@gmail.com>
2021-06-04 17:02:12 +0200

    add prove con js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-var km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 
-// età
-var età = parseInt(prompt('Quanti anni hai?'));
+// // età
+// var età = parseInt(prompt('Quanti anni hai?'));
 
-// operazione prezzo
-var biglietto = 0.21 * km;
+// // operazione prezzo
+// var biglietto = 0.21 * km;
 
-var scatola = biglietto;
+// var scatola = biglietto;
+
+var submit = document.getElementById('submit');
+submit.addEventListener("click", function(){
+    console.log('ciao')
+})
 
 // if else
-if(età < 18){
-    scatola = biglietto - (biglietto * 20) / 100;
-    scatola = scatola.toFixed(2);
-} else if(età > 65){
-    scatola = biglietto - (biglietto * 40) / 100;
-    scatola = scatola.toFixed(2);
-}
+// if(età < 18){
+//     scatola = biglietto - (biglietto * 20) / 100;
+//     scatola = scatola.toFixed(2);
+// } else if(età > 65){
+//     scatola = biglietto - (biglietto * 40) / 100;
+//     scatola = scatola.toFixed(2);
+// }
 
-// risultato prezzo
-console.log('scatola');
+// // risultato prezzo
+// console.log('scatola');
