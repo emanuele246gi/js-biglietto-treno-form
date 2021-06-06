abd29f745f4d2b1da4072ca6fb4ceec7edeffe5c
Emanuele <emanuele65b@gmail.com>
2021-06-06 13:01:33 +0200

    add calcolo (credo)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 // Input
-var nome = document.getElementById('nome');
 
-var km = document.getElementById("km");
+var km = parseInt(document.getElementById("km"));
 
-var età = document.getElementById("età");
+var età = parseInt(document.getElementById("età"));
 
-var submit = document.getElementById('submit');
-submit.addEventListener("click", function(){
-    document.getElementById('name').innerHTML = nome
+function submit(){
+    var nome = document.getElementById("nome").value;
+    document.getElementById("name").innerHTML = nome
+}
 
 var annulla = document.getElementById("annulla");
+    annulla.addEventListener("click", function(){
+    document.getElementById('name').innerHTML = '-';
+});
 
 
-// var biglietto = 0.21 * km;
+var biglietto = 0.21 * km;
 
-// var scatola = biglietto;
+var scatola = biglietto;
 
-})
 
 // if else
-// if(età < 18){
-//     scatola = biglietto - (biglietto * 20) / 100;
-//     scatola = scatola.toFixed(2);
-// } else if(età > 65){
-//     scatola = biglietto - (biglietto * 40) / 100;
-//     scatola = scatola.toFixed(2);
-// }
+if(età == minorenne){
+    scatola = biglietto - (biglietto * 20) / 100;
+    scatola = scatola.toFixed(2);
+} else if(età == Over65){
+    scatola = biglietto - (biglietto * 40) / 100;
+    scatola = scatola.toFixed(2);
+}
 
-// // risultato prezzo
-// console.log('scatola');
+// risultato prezzo
+document.getElementById('prezzo').innerHTML = scatola
